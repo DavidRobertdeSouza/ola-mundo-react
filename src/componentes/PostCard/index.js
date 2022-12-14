cf9aeeef3bb28a5b473842e0d0dc8b45ba6a4373
David Robert de Souza <david_robert97@outlook.com>
2022-12-14 19:33:47 -0300

    Curso finalizado

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,3 +1,4 @@
+import BotaoPrincipal from 'componentes/BotaoPrincipal'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Post.module.css'
@@ -8,7 +9,8 @@ export default function PostCard({post}) {
       <div className={styles.post}>
         <img className={styles.capa} src={`/assets/posts/${post.id}/capa.png`} alt='Imagem de capa do post' />
         <h2 className={styles.titulo}>{post.titulo}</h2>
-        <button className={styles.botaoLer}>Ler</button>
+
+        <BotaoPrincipal>Ler</BotaoPrincipal>
       </div>
     </Link>
   )
